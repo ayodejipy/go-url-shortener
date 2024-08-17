54f0391447ad88095440632d60d8d9d8c0c6198b
ayodejipy <ay.programmr@gmail.com>
2024-08-17 17:26:24 +0100

    fix: added docker yml file

--- internals/config/config.go
+++ internals/config/config.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
 
-func Config() {
-	fmt.Println("Setting new configuration files")
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Api_Version, Api_Title, Api_Description string
+	Redis_Port, Redis_Host, Redis_Ttl string
+	Db_Host, Db_Name, Db_User string
+	Db_Port, Db_Password string
+}
+
+func LoadConfig() *Config {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal("Cannot load environment file: ", err)
+	}
+
+	config := &Config{
+		Api_Version: os.Getenv("API_VERSION"),
+		Api_Title: os.Getenv("API_TITLE"),
+		Api_Description: os.Getenv("API_DESCRIPTION"),
+		Db_Host: os.Getenv("HOST"),
+		Db_Name: os.Getenv("DB_NAME"),
+		Db_Port: os.Getenv("DB_PORT"),
+		Db_User: os.Getenv("USERNAME"),
+		Db_Password: os.Getenv("PASSWORD"),
+	}
+
+	return config
 }
