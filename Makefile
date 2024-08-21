cd81c61141344ffd3de45017942ee9a8e13e5dbc
ayodejipy <ay.programmr@gmail.com>
2024-08-21 09:42:50 +0100

    fix: sqlc installed, migrations example ran

--- Makefile
+++ Makefile
@@ -1,3 +1,9 @@
+ifneq (,$(wildcard ./.env))
+	include .env
+	export
+endif
+
+
 build:
 	go build -o bin/api cmd/api/main.go && bin/api
 
@@ -11,4 +17,14 @@ down:
 	docker-compose down
 
 
-.PHONY: build run up down
+migratecreate:
+	migrate create -ext sql -dir $(MIGRATION_PATH) -seq $(name)
+
+migrateup:
+	migrate -path $(MIGRATION_PATH) -database "$(DATABASE_PSQL_URL)" -verbose up
+
+migratedown:
+	migrate -path $(MIGRATION_PATH) -database "$(DATABASE_PSQL_URL)" -verbose down
+
+
+.PHONY: build run up down migratecreate migrateup migratedown
---
+++ internals/db/migration/000001_init_migration.up.sql
@@ -0,0 +1,27 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE "users" (
+	"id" uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+	"email" VARCHAR(100) UNIQUE NOT NULL,
+	"first_name" VARCHAR(200) NOT NULL,
+	"last_name" VARCHAR(200) NOT NULL,
+	"password_hash" VARCHAR NOT NULL,
+	"role" VARCHAR(50) DEFAULT 'user',
+	"created_at" TIMESTAMP NOT NULL DEFAULT (NOW()),
+	"updated_at" TIMESTAMP NOT NULL DEFAULT (NOW()),
+	"deleted_at" TIMESTAMP NULL
+);
+
+CREATE TABLE "urls" (
+	"id" uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+	"original_url" TEXT NOT NULL,
+	"short_code" VARCHAR(20) UNIQUE NOT NULL,
+	"click_count" INT DEFAULT 0,
+	"is_active" BOOLEAN DEFAULT TRUE,
+	-- "user_id" uuid REFERENCES users("id"),
+	"created_at" TIMESTAMP NOT NULL DEFAULT (NOW()),
+)
+
+
+ALTER TABLE "urls" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id")
+
